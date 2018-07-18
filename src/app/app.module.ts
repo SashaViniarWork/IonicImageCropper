fba556b4539b07dca0eaae468f1a758a1563e26f
SashaViniarWork <alexander.viniar@etiya.com>
2018-07-18 14:09:52 +0300

    Crop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { PhotoProvider } from '../providers/photo/photo';
+import { Camera } from '@ionic-native/camera';
+import { ImageCropperModule } from "ng2-img-cropper/index";
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +17,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    ImageCropperModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +29,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Camera,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PhotoProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {ImageCropperComponent, CropperSettings, Bounds} from 'ng2-img-cropper';
+import {PhotoProvider} from '../../providers/photo/photo';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('cropper') ImageCropper: ImageCropperComponent;
+  public cropperSettings;
+  public croppedWidth: Number;
+  public croppedHeight: Number;
+  public data: any;
+  public canSave: boolean = false;
 
+  constructor(public navCtrl: NavController, private _PHOTO: PhotoProvider) {
+    this.cropperSettings = new CropperSettings();
+    this.cropperSettings.noFileInput = true;
+    this.cropperSettings.cropOnResize = true;
+    this.cropperSettings.fileType = 'image/jpeg';
+    this.cropperSettings.keepAspect = false;
+    this.data = {};
   }
+
+  handleCropping(bounds: Bounds) {
+    this.croppedHeight = bounds.bottom - bounds.top;
+    this.croppedWidth = bounds.right - bounds.left;
+    this.canSave = true;
+  }
+
+  selectImage() {
+    this.canSave = false;
+    this._PHOTO.selectImage()
+      .then((data: any) => {
+        let image: any = new Image();
+        image.src = data;
+        this.ImageCropper.setImage(image);
+      })
+      .catch((error: any) => {
+        console.dir(error);
+      });
+  }
+
+  saveImage() {
+    console.dir(this.data.image);
+  }
+
 
 }
